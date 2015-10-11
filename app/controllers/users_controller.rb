1f4b3e44e36a6a24228ef1abbc707940a774b696
Tristan Rubadeau <tristan@rubadeau.com>
2015-10-11 14:17:38 -0600

    user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
 
-  # def create
-  #   @user = User.new(user_params)
-  #   if @user.save
-  #     flash[:success] = "User was successfully created"
-  #     redirect_to root_path
-  #   else
-  #     render :new
-  #   end
-  # end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "User was successfully created"
+      redirect_to dashboard_path(current_user)
+    else
+      render :new
+    end
+  end
 
   def edit
     @user = User.find(params[:id])
@@ -17,12 +17,6 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-
-      # GET https://bufferapp.com/oauth2/authorize?
-      # client_id="1/b2043f218b85328de2e91378ba6b2a1f"&
-      # redirect_uri="http://localhost:3000/"&
-      # response_type=code
-
       redirect_to root_path, notice: 'Picture Added!'
     else
       render :edit
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
 
   resources :users
 
+  resources :dashboards
+
   resources :buffers
 
 end
